59661c67b00d436ff0af6471a379fb51630e17a3
redbnav <redbnav@gmail.com>
2017-11-30 11:31:10 -0600

    todo web app

--- first/src/app/app.module.ts
+++ first/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ActivationEnd } from '@angular/router/src/events';
+import { Action } from 'rxjs/scheduler/Action';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -20,3 +22,12 @@ import { ProductComponent } from './product/product.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+var model = { 
+  user: "Naveen",
+  items: [
+    {action: "Explore Angular", done: true },
+    {action: "CRUD using Angular", done: false},
+    {actons: "Events in Angular", done: true},
+    {action: "commit it git repo", done: true}]
+  }
