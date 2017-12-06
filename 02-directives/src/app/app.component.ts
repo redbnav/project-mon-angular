7ebe243cc50b41f87ce6edae19a599ffcf2d04ee
redbnav <redbnav@gmail.com>
2017-12-06 08:49:27 -0600

    removed submodule

---
+++ 02-directives/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dir-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private switch = true;
+  onSwitch(){
+    this.switch = !this.switch;
+  }
+}
